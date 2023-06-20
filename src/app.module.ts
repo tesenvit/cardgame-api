37058b8bd1bbdfe291ca99eb67eff5a15e6a25ff
vitaliy.balaev <vitaliy.balaev@wildix.com>
2023-06-20 18:33:00 +0200

    connect to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
+import { UsersModule } from './users/users.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+      ConfigModule.forRoot({
+          envFilePath: `.${process.env.NODE_ENV}.env`
+      }),
+      MongooseModule.forRoot(
+          `mongodb://${process.env.MONGODB_HOST}/${process.env.MONGODB_DB}`
+      ),
+      UsersModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT
+  const app = await NestFactory.create(AppModule)
+
+  await app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 }
-bootstrap();
+bootstrap()
